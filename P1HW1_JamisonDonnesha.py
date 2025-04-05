005ea7ec00be1fa59413dfeb2f7924928142511a
DJami0205 <jamisond1501@student.faytechcc.edu>
2025-04-05 16:21:20 -0400

    Add files via upload

---
+++ P1HW1_JamisonDonnesha.py
@@ -0,0 +1,14 @@
+print("DonneshaJamison, 4/5/2025, PLHW1,learning how to code exponenets as well as adding and subtracting.")
+
+print("----- Calculating Exponenets----")
+integer=input("Enter an integer as the base value: ")
+integer=input("Enter an integer as an exponent:")
+print( "7 raised to the power of 3 is 343 !!" )
+
+print("-----Addition and Subtraction----")
+
+integer=input("Enter a starting integer:")
+integer=input("Enter an integer to add:")
+integer=input("Enter an integer to subtract:")
+
+print("10+ 6 - 4 equal to 12")
