640b6ff915a72cb809237246230d2bc8431ee56b
DJami0205 <jamisond1501@student.faytechcc.edu>
2025-04-05 16:19:14 -0400

    Add files via upload

---
+++ P1HW2_JamisonDonnesha.py
@@ -0,0 +1,17 @@
+print("DonneshaJamison, 4/5/2025, P1LHW2, This program calculatex and displays travel")
+integer=input("Enter Budget:")
+integer=input("Enter travel destination:")
+integer=input("How much wdo you think you will spend on gas?")
+interger=input("Approximately, How much will you need for acconmodation/hotel?")
+integer=input("Last, how much do you need for food?:")
+
+
+print("--------Travel Expenses--------")
+
+integer=input("location:")
+integer=input("Intial Budget:")
+integer=input("Fuel")
+integer=input("Accomodation:")
+integer=input("food")
+
+input=print("remaining balance")
